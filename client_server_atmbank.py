950271e46e208d91e10906c8384783ec26dc22d2
Michael M <mimeng2518@gmail.com>
2025-04-12 17:46:29 -0400

    working file, going to use one file that odes both

---
+++ client_server_atmbank.py
@@ -0,0 +1,13 @@
+
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1] == "server":
+        #server = bank()
+        #server.start()
+        exit
+    else:
+        #client = atm()
+        #client.connect()
+        exit
